9c4ba4d18c7fbc6dd77e1fc11d41d7a762a289eb
Na Hongyan <547840062@qq.com>
2024-04-23 23:52:39 +0800

    修改提交

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sanic_cache',
+    version='V0.1.0',
+    packages=['sanic_cache'],
+    url='https://github.com/nahongyan1997/sanic_cache',
+    license='MIT',
+    author='Na Haha',
+    author_email='547840062@qq.com',
+    description='flask-cache -> sanic-cache'
+)
