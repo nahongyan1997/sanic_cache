5351042dbe465411e5235ce20c1a1d7672ffa899
Na Hongyan <547840062@qq.com>
2024-04-24 11:27:57 +0800

    修改结构

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sanic_cache',
-    version='V0.1.0',
-    packages=['sanic_cache'],
+    version='0.1.0',
+    packages=["sanic_cache"],
     url='https://github.com/nahongyan1997/sanic_cache',
     license='MIT',
     author='Na Haha',
     author_email='547840062@qq.com',
-    description='flask-cache -> sanic-cache'
+    description='flask-cache -> sanic-cache',
+    install_requires = [
+        'sanic',
+        "sanic_ext",
+        "redis-py"
+    ],
 )
